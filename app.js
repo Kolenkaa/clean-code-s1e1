89023a68e02a7941c84e9a14d37b5bcb39009e4c
Kolenkaa <kkolenkaa@gmail.com>
2022-05-26 18:13:21 +0300

    refactor: correct tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,19 +32,24 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+	 listItem.className = "task-item";
+
     label.innerText=taskString;
-    label.className='task';
+    label.className="label task";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+	 checkBox.className = "input input-checkbox";
+
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input input-task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+	 deleteButtonImg.className="img";
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -82,7 +87,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector("input-task");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
